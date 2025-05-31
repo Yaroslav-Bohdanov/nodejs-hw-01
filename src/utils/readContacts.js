ab410064c54d4b6e961a774e6930053048166f9d
Ярослав Богданов <bogdanov.yar99@gmail.com>
2025-05-31 13:32:52 +0300

    init

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    if (!data.trim()) return [];
+    return JSON.parse(data);
+  } catch (err) {
+    if (err.code === 'ENOENT') return [];
+    console.error('Помилка зчитування файлу:', err);
+    return [];
+  }
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  const contactsToWrite = Array.isArray(updatedContacts) ? updatedContacts : [];
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify(contactsToWrite, null, 2));
+  } catch (err) {
+    console.error('Помилка запису файлу:', err);
+    throw err;
+  }
+};
